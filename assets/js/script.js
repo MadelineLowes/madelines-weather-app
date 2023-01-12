d42a3802ce5ed6b04bd94412cd60b22c6762b63b
MadelineLowes <madeline.lowes@gmail.com>
2023-01-12 18:11:57 -0500

    updated styling to accomodate various screen sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@ let API_KEY = "cd449ce8a0596130f95722331fe56ab4"
 
 function sidebarBtn(i) {
     var searchValue = $(`#city-list${i}`).attr("value");
+    // call function with defined searchValue
     fetchBySearch(searchValue);
 }
 
 function searchbarBtn() {
     var searchValue = $("#searchInput").val();
+    // call function with defined searchValue
     fetchBySearch(searchValue);
 }
 
@@ -14,11 +16,9 @@ function fetchBySearch(searchValue) {
     let fetchByName = `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=1&lang=en&appid=${API_KEY}`
     fetch(fetchByName)
         .then(function (response) {
-            console.log(response);
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             if (data.length > 0) {
                 let latitude = data[0].lat;
                 let longitude = data[0].lon;
@@ -41,14 +41,14 @@ function fetchBySearch(searchValue) {
                             $(`#icon${i}`).attr("src", `${iconSrc}`)
                             // set the temp
                             let temp = data.daily[i].temp.day;
-                            $(`#temp${i}`).html(`temp: ${temp} C`);
+                            $(`#temp${i}`).html(`Temp: ${temp} C`);
                             // set the wind
                             let wind = data.daily[i].wind_speed;
-                            $(`#wind${i}`).html(`wind: ${wind} km/h`);
+                            $(`#wind${i}`).html(`Wind: ${wind} km/h`);
                             // set the humidity
                             let humidity = data.daily[i].humidity;
-                            $(`#humidity${i}`).html(`humdity: ${humidity}%`);
-                            // saving city's forecast to local storage
+                            $(`#humidity${i}`).html(`Humdity: ${humidity}%`);
+                            // save city's forecast to local storage
                             let cityForecast = {
                                 date: unixFormat,
                                 icon: iconSrc,
@@ -58,7 +58,7 @@ function fetchBySearch(searchValue) {
                             }
                             localStorage.setItem(`searchedCityForecast${i}`, JSON.stringify(cityForecast));
                         }
-                        // saving city name to local storage
+                        // save city name to local storage
                         let searchedCityName = {
                             name: searchValue,
                         }
@@ -66,7 +66,7 @@ function fetchBySearch(searchValue) {
                         // this page reload is to ensure that the new city name gets added to the sidebar list
                         location.reload(true);
                     })
-                // getting searched cities from local storage, creating an array, adding new city to the array, and then setting in local storage again for sidebar list
+                // get searched cities from local storage, create an array, add new city to the array, and then set in local storage again for sidebar list
                 var searchedCities = JSON.parse(localStorage.getItem("searchedCityList")) || [];
 
                 // this is only executed if the searchValue doesn't already exist in local storage
@@ -91,29 +91,26 @@ function currentCitySearch() {
 
         for (i = 0; i < 6; i++) {
             const city = JSON.parse(localStorage.getItem(`searchedCityForecast${i}`));
-
             // set the title
             $(`#title${i}`).html(city.date);
             // set the icon
             $(`#icon${i}`).attr("src", city.icon)
             // set the temp
-            $(`#temp${i}`).html(`temp: ${city.temp} C`);
+            $(`#temp${i}`).html(`Temp: ${city.temp} C`);
             // set the wind
-            $(`#wind${i}`).html(`wind: ${city.wind} km/h`);
+            $(`#wind${i}`).html(`Wind: ${city.wind} km/h`);
             // set the humidity
-            $(`#humidity${i}`).html(`humdity: ${city.humidity}%`);
+            $(`#humidity${i}`).html(`Humdity: ${city.humidity}%`);
         }
     }
 }
 
 function storedCities() {
-    // getting searched cities from local storage, creating an array, and creating button elements for sidebar list
+    // get searched cities from local storage, create an array, and create button elements for sidebar list
     const searchedCities = JSON.parse(localStorage.getItem("searchedCityList")) || [];
 
     for (i = 0; i < searchedCities.length; i++) {
-        $("#cityList").append('<li><button class="cityListBtn" id=' + `city-list${i}` + ' type="button" value=' + searchedCities[i] + ' onclick="sidebarBtn(' + i + ')">' + searchedCities[i] + '</button></li>');
-        // value='+`${searchedCities[i]}`+'
-        // DONT ADD TO LIST IF VALUE IS BLANK
+        $("#cityList").append('<li><button class="cityListBtn" style="font-family: Merriweather, serif" id=' + `city-list${i}` + ' type="button" value="' + searchedCities[i] + '" onclick="sidebarBtn(' + i + ')">' + searchedCities[i] + '</button></li>');
     }
 }
 
